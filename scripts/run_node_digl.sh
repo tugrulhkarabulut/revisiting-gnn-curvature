970f47567e11abdf5abfc58acd2ed1f7dfd57ce4
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2025-07-19 18:08:12 +0300

    digl added

---
+++ scripts/run_node_digl.sh
@@ -0,0 +1,41 @@
+python run_graph_classification.py --rewiring=digl --dataset=cora --layer_type=GCN --device=0 --num_trials=50 --alpha 0.15 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=cora --layer_type=GCN --device=0 --num_trials=50 --alpha 0.15 --eps 1e-4
+python run_graph_classification.py --rewiring=digl --dataset=cora --layer_type=GCN --device=0 --num_trials=50 --alpha 0.10 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=cora --layer_type=GCN --device=0 --num_trials=50 --alpha 0.10 --eps 1e-4
+python run_graph_classification.py --rewiring=digl --dataset=cora --layer_type=GCN --device=0 --num_trials=50 --alpha 0.05 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=cora --layer_type=GCN --device=0 --num_trials=50 --alpha 0.05 --eps 1e-4
+
+python run_graph_classification.py --rewiring=digl --dataset=citeseer --layer_type=GCN --device=0 --num_trials=50 --alpha 0.15 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=citeseer --layer_type=GCN --device=0 --num_trials=50 --alpha 0.15 --eps 1e-4
+python run_graph_classification.py --rewiring=digl --dataset=citeseer --layer_type=GCN --device=0 --num_trials=50 --alpha 0.10 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=citeseer --layer_type=GCN --device=0 --num_trials=50 --alpha 0.10 --eps 1e-4
+python run_graph_classification.py --rewiring=digl --dataset=citeseer --layer_type=GCN --device=0 --num_trials=50 --alpha 0.05 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=citeseer --layer_type=GCN --device=0 --num_trials=50 --alpha 0.05 --eps 1e-4
+
+python run_graph_classification.py --rewiring=digl --dataset=texas --layer_type=GCN --device=0 --num_trials=50 --alpha 0.15 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=texas --layer_type=GCN --device=0 --num_trials=50 --alpha 0.15 --eps 1e-4
+python run_graph_classification.py --rewiring=digl --dataset=texas --layer_type=GCN --device=0 --num_trials=50 --alpha 0.10 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=texas --layer_type=GCN --device=0 --num_trials=50 --alpha 0.10 --eps 1e-4
+python run_graph_classification.py --rewiring=digl --dataset=texas --layer_type=GCN --device=0 --num_trials=50 --alpha 0.05 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=texas --layer_type=GCN --device=0 --num_trials=50 --alpha 0.05 --eps 1e-4
+
+python run_graph_classification.py --rewiring=digl --dataset=cornell --layer_type=GCN --device=0 --num_trials=50 --alpha 0.15 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=cornell --layer_type=GCN --device=0 --num_trials=50 --alpha 0.15 --eps 1e-4
+python run_graph_classification.py --rewiring=digl --dataset=cornell --layer_type=GCN --device=0 --num_trials=50 --alpha 0.10 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=cornell --layer_type=GCN --device=0 --num_trials=50 --alpha 0.10 --eps 1e-4
+python run_graph_classification.py --rewiring=digl --dataset=cornell --layer_type=GCN --device=0 --num_trials=50 --alpha 0.05 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=cornell --layer_type=GCN --device=0 --num_trials=50 --alpha 0.05 --eps 1e-4
+
+python run_graph_classification.py --rewiring=digl --dataset=wisconsin --layer_type=GCN --device=0 --num_trials=50 --alpha 0.15 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=wisconsin --layer_type=GCN --device=0 --num_trials=50 --alpha 0.15 --eps 1e-4
+python run_graph_classification.py --rewiring=digl --dataset=wisconsin --layer_type=GCN --device=0 --num_trials=50 --alpha 0.10 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=wisconsin --layer_type=GCN --device=0 --num_trials=50 --alpha 0.10 --eps 1e-4
+python run_graph_classification.py --rewiring=digl --dataset=wisconsin --layer_type=GCN --device=0 --num_trials=50 --alpha 0.05 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=wisconsin --layer_type=GCN --device=0 --num_trials=50 --alpha 0.05 --eps 1e-4
+
+python run_graph_classification.py --rewiring=digl --dataset=chameleon --layer_type=GCN --device=0 --num_trials=50 --alpha 0.15 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=chameleon --layer_type=GCN --device=0 --num_trials=50 --alpha 0.15 --eps 1e-4
+python run_graph_classification.py --rewiring=digl --dataset=chameleon --layer_type=GCN --device=0 --num_trials=50 --alpha 0.10 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=chameleon --layer_type=GCN --device=0 --num_trials=50 --alpha 0.10 --eps 1e-4
+python run_graph_classification.py --rewiring=digl --dataset=chameleon --layer_type=GCN --device=0 --num_trials=50 --alpha 0.05 --eps 1e-3
+python run_graph_classification.py --rewiring=digl --dataset=chameleon --layer_type=GCN --device=0 --num_trials=50 --alpha 0.05 --eps 1e-4
