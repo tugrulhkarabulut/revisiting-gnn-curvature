29f8c9f3a56fa65309a090a8be7727ee803b1174
Tuğrul Hasan Karabulut <tugrulkarabulut64@gmail.com>
2025-07-28 00:32:39 +0300

    gcn script

---
+++ scripts/run_node_gcn.sh
@@ -0,0 +1,6 @@
+python run_node_classification.py     --rewiring none     --layer_type GCN    --num_trials 50    --device cuda:0     --dataset cora
+python run_node_classification.py     --rewiring none     --layer_type GCN    --num_trials 50    --device cuda:0     --dataset citeseer
+python run_node_classification.py     --rewiring none     --layer_type GCN    --num_trials 50    --device cuda:0     --dataset texas
+python run_node_classification.py     --rewiring none     --layer_type GCN    --num_trials 50    --device cuda:0     --dataset cornell
+python run_node_classification.py     --rewiring none     --layer_type GCN    --num_trials 50    --device cuda:0     --dataset wisconsin
+python run_node_classification.py     --rewiring none     --layer_type GCN    --num_trials 50    --device cuda:0     --dataset chameleon
